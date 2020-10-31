2cab2876a9310116b6a1c00b05d6647b5d4f3942
Ashley Kualii <ashkua10@gmail.com>
2020-10-30 20:14:49 -0700

    connect recent posts to redux and setup redux-thunk for api requests

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,6 @@
-import { combineReducers } from "redux";
-
-import { reducer as Form } from "redux-form";
-
-const rootReducer = combineReducers({
-  Form,
-});
-
-export default rootReducer;
+export function fetchRecentPosts() {
+  return function (dispatch) {
+    //perform our request in here.
+    console.log("hello");
+  };
+}
